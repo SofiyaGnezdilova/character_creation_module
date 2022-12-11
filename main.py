4e33cc712c3260b2c24982f504b88cd4f8825946
Sofiya Gnezdilova <sofie_p@mail.ru>
2022-12-11 23:27:12 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    '''Attack.'''
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 + randint(3,5)}')
     if char_class == 'mage':
@@ -14,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    '''Defence.'''
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5,10)} урона')
     if char_class == 'mage':
@@ -24,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    '''Special features.'''
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость'
                 f'{80+25}»')
@@ -35,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    '''Training.'''
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 def choice_char_class() -> str:
     approve_choice: str = None
+    '''Class.'''
     char_class: str = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь '
@@ -80,7 +88,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
